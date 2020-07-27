13a67e0dc5ea00b56868bca8fa63c7d78ae578b5
19012079 <19012079@MingHao-MacBookPro>
2020-07-27 18:38:30 +0800

    Fixed Liang Kok's Option 1

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -134,32 +134,36 @@ public class ResourceCentre {
 					ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable()),
 					camcorderList.get(i).getDueDate(),camcorderList.get(i).getOpticalZoom());
 		}
-		return output;
+			return output;
+			
 	}
 	public static void viewAllCamcorder(ArrayList<Camcorder> camcorderList) {
 		ResourceCentre.setHeader("CAMCORDER LIST");
-		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION",
-				"AVAILABLE", "DUE DATE","OPTICAL ZOOM");
-		 output += retrieveAllCamcorder(camcorderList);	
+		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION","AVAILABLE", "DUE DATE","OPTICAL ZOOM");
+		output += retrieveAllCamcorder(camcorderList);	
 		System.out.println(output);
 	}
 
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
-		for (int i = 0; i<chromebookList.size();i++) {
-			
-		output += String.format("%-10s %-30s %-10s %-10s %-20s \n"),
-				chromebookList.get(i).getAssetTag(),,chromebookList.get(i).getDescription(),
-				ResourceCentre.showAvailability(chromebookList.get(i).getAvailable()),chromebookList.get(i).getDueDate(),
-				chromebookList.get(i).getOs());
+		for (int i = 0; i < chromebookList.size(); i++) {
+				
+			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
+					chromebookList.get(i).getDescription(), 
+					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
+					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
 		}
+			
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
-		
-		String output = retrieveAllChromebook(chromebookList);
+			
+		ResourceCentre.setHeader("CHROMEBOOK LIST");
+		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION","AVAILABLE", "DUE DATE","OPERATING SYSTEM");
+		output += retrieveAllChromebook(chromebookList);
 		System.out.println(output);
 	}
+
 
 	//================================= Option 2 Add =================================
 	public static Camcorder inputCamcorder() {
